bbdb770d9216d55c9c609e084fb022fc056b50b9
payalkaramunge <karamungepayal@gmail.com>
2024-02-17 12:09:34 +0530

    new commit

---
+++ Variable Assignment/variable_Assignment3.java
@@ -0,0 +1,9 @@
+public class variable_Assignment3 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		int a = 10;
+                int b = 20;
+                int c = a+b;
+                System.out.println(c);
+	}
+}
