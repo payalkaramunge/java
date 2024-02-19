02610caf5cb95aa120a05e399b5c271c30998f02
payalkaramunge <karamungepayal@gmail.com>
2024-02-19 14:09:35 +0530

    new commit

--- Variable Assignment/variable_Assignment3.java
+++ Variable Assignment/variable_Assignment3.java
@@ -1,5 +1,6 @@
+
 public class variable_Assignment3 {
-    public static void main (String[] args) throws java.lang.Exception
+public static void main (String[] args) throws java.lang.Exception
 	{
 		int a = 10;
                 int b = 20;
---
+++ Variable Assignment/variable_Assignment4.java
@@ -0,0 +1,10 @@
+public class variable_Assignment4 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		int a = 1042;
+                int b = 7;
+                int quo = a/b;
+                int rem = a%b;
+                System.out.println(quo + " " + rem);
+	}
+}
