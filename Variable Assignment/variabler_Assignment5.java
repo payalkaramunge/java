69d997019f019964d1b86029d74bf725cefa5ab1
payalkaramunge <karamungepayal@gmail.com>
2024-02-20 08:56:58 +0530

    new commit

---
+++ Variable Assignment/variabler_Assignment5.java
@@ -0,0 +1,11 @@
+public class variabler_Assignment5 {
+    
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		float v1 = 10.5f;
+                float v2 = 12.5f;
+                float v3 = 12.15f;
+                float average = (v1+v2+v3)/3;
+                System.out.println(average);
+	}
+}
