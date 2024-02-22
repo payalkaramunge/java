049912920ab4339645ddd1dbd0be8a6256286cac
payalkaramunge <karamungepayal@gmail.com>
2024-02-22 16:02:50 +0530

    new commit

---
+++ Variable Assignment/Hello_World.java
@@ -0,0 +1,10 @@
+public class Hello_World {
+    
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Hello, World.");
+                System.out.println("Hello, Java.");
+    
+	}
+    
+}
