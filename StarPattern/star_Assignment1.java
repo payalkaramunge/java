6095167c4603d5c23d42d20dfd6c938749530a83
payalkaramunge <karamungepayal@gmail.com>
2024-02-24 10:32:49 +0530

    new commit

---
+++ StarPattern/star_Assignment1.java
@@ -0,0 +1,7 @@
+public class star_Assignment1 {
+     
+        public static void main (String[] args) throws java.lang.Exception
+        {
+        System.out.println("*\n***\n*****");
+        }
+}
