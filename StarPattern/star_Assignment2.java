5c10e66501d25174c415098249399b86e918ce04
payalkaramunge <karamungepayal@gmail.com>
2024-02-25 12:21:57 +0530

    new commit

---
+++ StarPattern/star_Assignment2.java
@@ -0,0 +1,11 @@
+public class star_Assignment2 {
+    
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("* * * * *");
+                System.out.println("* \n* \n* \n* \n*");
+    
+    
+    //System.out.println("* * * * * \n*\n*\n*\n*\n*");
+	}
+}
