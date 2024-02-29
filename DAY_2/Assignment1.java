72ce2a700affd8e0ce9fcd9ce1c3ed88bf8f8837
payalkaramunge <karamungepayal@gmail.com>
2024-02-29 09:42:57 +0530

    new commit

--- DAY_2/Assignment1.java
+++ DAY_2/Assignment1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Assignment1 {
     public static void main (String[] args) throws java.lang.Exception
--- DAY_2/Assignment2.java
+++ DAY_2/Assignment2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Assignment2 {
 public static void main (String[] args) throws java.lang.Exception
 	{
