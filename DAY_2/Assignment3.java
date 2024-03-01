8e5a26b81f2814d2d759d4fcdc93181fe6b96802
payalkaramunge <karamungepayal@gmail.com>
2024-03-01 11:21:16 +0530

    new commit

---
+++ DAY_2/Assignment3.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Assignment3
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+                int A = sc.nextInt();
+                int B = sc.nextInt();
+                System.out.println(A+B);
+                System.out.println(A-B);
+                System.out.println(A*B);
+                System.out.println(A/B);
+                System.out.println(A%B);
+	}
+}
