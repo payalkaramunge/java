0ea8adef908e3a7f8330f68bf39510404e3ca482
payalkaramunge <karamungepayal@gmail.com>
2024-03-06 18:26:49 +0530

    new commit

--- DAY_2/Assignment2.java
+++ DAY_2/Assignment2.java
@@ -8,6 +8,7 @@ public static void main (String[] args) throws java.lang.Exception
     int a = scanner.nextInt();
     int b = scanner.nextInt();
     int c = scanner.nextInt();
+    
     System.out.println(a);
     System.out.println(b);
     System.out.println(c);
