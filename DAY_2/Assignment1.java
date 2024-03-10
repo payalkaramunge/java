27e5a2df93c939f542924758211b299418bef689
payalkaramunge <karamungepayal@gmail.com>
2024-03-10 14:44:54 +0530

    new commit

--- DAY_2/Assignment1.java
+++ DAY_2/Assignment1.java
@@ -4,6 +4,7 @@ public class Assignment1 {
     public static void main (String[] args) throws java.lang.Exception
 	{
         System.out.println("Enter your value fahrenheit: ");
+        
 		Scanner scanner = new Scanner(System.in);
                 int fahrenheit = scanner.nextInt();
                 int celsius = (((fahrenheit-32)*5)/9);
