10b77c1fbffa610d67dbc1783f90daf31abfa3af
payalkaramunge <karamungepayal@gmail.com>
2024-03-21 08:20:32 +0530

    new commit

--- DAY_2/Assignment2.java
+++ DAY_2/Assignment2.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Assignment2 {
+  
 public static void main (String[] args) throws java.lang.Exception
 	{
     System.out.println("Enter your value of a,b,c: ");
