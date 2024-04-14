42674d7178647cb4637a28ca4e93a8ca46e0066e
payalkaramunge <karamungepayal@gmail.com>
2024-04-14 13:35:15 +0530

    new commit

--- DAY_2/Assignment1.java
+++ DAY_2/Assignment1.java
@@ -1,3 +1,7 @@
+// Given a temperature, of F in Fahrenheit, your task is to convert it into Celsius using the following equation:-
+// T(°C) = (T(°F) - 32)*5/9
+// Note: It is guaranteed that F - 32 will be a multiple of 9.
+
 import java.util.Scanner;
 
 public class Assignment1 {
